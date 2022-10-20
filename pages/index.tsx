cc28b360bdbf83f1e46912632e0f2c8f0d741a10
M1-Masa <masa455.wv@gmail.com>
2022-10-20 20:13:56 +0900

    add:actions test

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          synchronize edit to <a href="https://nextjs.org">Next.js!</a>
+          synchronize 01 to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
