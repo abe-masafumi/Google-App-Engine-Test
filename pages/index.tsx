a7afea133e1830f7d04964b691b3810098d34839
@Masa455 <83285963+abe-masafumi@users.noreply.github.com>
2022-12-06 07:44:51 +0000

    add:commetn test

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          hello 課金 to <a href="https://nextjs.org">Next.js!</a>
+          hello comment to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
